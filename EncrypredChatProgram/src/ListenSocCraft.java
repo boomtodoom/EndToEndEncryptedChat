363b1a5f9a99f54ac9292fe10ffe6670ed0c1fe7
sm13g21 <sm13g21@soton.ac.uk>
2021-11-13 18:59:05 +0000

    Added new thread class that will creat a new socket when someone new connects using your IP.

--- EncrypredChatProgram/src/ListenSocCraft.java
+++ EncrypredChatProgram/src/ListenSocCraft.java
@@ -1,8 +1,29 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ListenSocCraft extends Thread{
+  private ServerSocket servSoc;
+  private ArrayList<Socket> socketArray;
 
 
+
+  public ListenSocCraft(ServerSocket servSoc, ArrayList<Socket> arrList ){
+    this.socketArray = arrList;
+    this.servSoc = servSoc;
+  }
 
   public void run(){
+    while(true){
+      try {
+        Socket newSoc = servSoc.accept();
 
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+
+    }
   }
 }
--- EncrypredChatProgram/src/Receive.java
+++ EncrypredChatProgram/src/Receive.java
@@ -4,24 +4,37 @@ import java.net.Socket;
 import java.util.Stack;
 import javax.xml.crypto.Data;
 
+
+/**
+ * class is a thread that is run for each connection that is made.
+ * It listens for incoming messages and saves them in a socket
+ */
+
 public class Receive extends Thread {
   private Socket clientSocket;
   private DataInputStream input;
   private String currentMessage = null;
   private Stack<String> messageStack;
 
+  /**
+  * Constructior taks a socket
+   * */
   public Receive(Socket sock) throws IOException {
     clientSocket = sock;
     input = new DataInputStream(sock.getInputStream());
   }
 
-
+  /**
+  * Getter grabs socket stack
+   */
   public Stack<String> getStack(){
     return messageStack;
   }
 
 
-
+  /**
+  * Method runs repetedly due to loop. It listens for a message and will save it in the stack
+   */
   public void run() {
     while (true) {
 
