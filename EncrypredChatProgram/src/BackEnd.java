b98ca9f2a6803426c20f1e7e0691a3122f265959
sm13g21 <sm13g21@soton.ac.uk>
2021-11-13 17:57:54 +0000

    Added a socket Listner thread class that accepts new connections from peaople

--- EncrypredChatProgram/src/BackEnd.java
+++ EncrypredChatProgram/src/BackEnd.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -7,14 +9,21 @@ public class BackEnd {
   private ArrayList<Socket> sockArray = new ArrayList<>();
   private ArrayList<Send> senderArray = new ArrayList<>();
 
+
   public static void main(String[] args) throws IOException {
     Scanner scan = new Scanner(System.in);
     System.out.println("Please enter the ip you wish to connect to: ");
     String ip = scan.nextLine();
+    InetAddress iAddress = InetAddress.getByName(ip);
     System.out.println("Please enter the port you wish to connect to: ");
     int port = scan.nextInt();
-    Socket socket = new Socket(ip,port);
-    socket.
+    Socket socket = new Socket(iAddress, port);
+    System.out.println("Please enter a port you want open");
+    int openPort = scan.nextInt();
+
+    ServerSocket sSoc = new ServerSocket(openPort);
+    Socket newSoc = sSoc.accept();
+
     Send initMsg = new Send(socket);
     System.out.println("Please enter your desired username");
     String uName = scan.nextLine();
---
+++ EncrypredChatProgram/src/ListenSocCraft.java
@@ -0,0 +1,8 @@
+public class ListenSocCraft extends Thread{
+
+
+
+  public void run(){
+
+  }
+}
