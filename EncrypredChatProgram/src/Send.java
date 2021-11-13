73077ea6fef4718e926dc3e022b4c6d914d744a9
boomtodoom <65396048+boomtodoom@users.noreply.github.com>
2021-11-13 14:45:19 +0000

    Created basic send class with a constructor that takes the socket in and initialises an output stream, and a method that sends a message through the output stream.

--- EncrypredChatProgram/src/Send.java
+++ EncrypredChatProgram/src/Send.java
@@ -1,3 +1,32 @@
-public class Send {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
+public class Send {
+  private String message;
+  private Socket clientSocket;
+  private DataOutputStream output;
+  //public key here
+
+  /**
+   *Constructs the class taking a socket as an input and initialising an output stream
+   * @param sock
+   * Socket input that is used to initialise the output stream
+   */
+  public Send(Socket sock) throws IOException {
+    clientSocket=sock;
+    output = new DataOutputStream(clientSocket.getOutputStream());
+  }
+
+  /**
+   * Sends a message along the data output stream.
+   * @param msg
+   * String inpput that is the message to be sent.
+   * @throws IOException
+   * Throws an error if the output fails
+   */
+  void send(String msg) throws IOException {
+    message=msg;
+    output.writeUTF(message);
+  }
 }
