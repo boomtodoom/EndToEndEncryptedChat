1ba9f376678ee02c0d09261d2a8da06103639d67
j-syl-klaxon <jsylvester2k@outlook.com>
2021-11-13 22:11:35 +0000

    fix: add boolean property to check message encrypt status

--- EncrypredChatProgram/src/ChatMessage.java
+++ EncrypredChatProgram/src/ChatMessage.java
@@ -1,9 +1,18 @@
 public class ChatMessage {
     private static int userHashCode;
     private static byte[] message;
+    private static boolean messageIsEncrypted;
 
     ChatMessage(int userHashCode, byte[] message){
         this.userHashCode = userHashCode;
         this.message = message;
+    }
+
+    public static void setMessageIsEncrypted(boolean messageIsEncrypted) {
+        ChatMessage.messageIsEncrypted = messageIsEncrypted;
+    }
+
+    public static boolean getMessageIsEncrypted() {
+        return messageIsEncrypted;
     }
 }
