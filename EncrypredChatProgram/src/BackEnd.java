c39b37b76a0dc8bd6c0510b0c4a5b8d22e2d0569
boomtodoom <65396048+boomtodoom@users.noreply.github.com>
2021-11-13 16:52:00 +0000

    Fixed sams stupid error pt3

--- EncrypredChatProgram/src/BackEnd.java
+++ EncrypredChatProgram/src/BackEnd.java
@@ -1,11 +1,13 @@
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BackEnd {
   private ArrayList<Socket> sockArray = new ArrayList<>();
   private ArrayList<Send> senderArray = new ArrayList<>();
 
   public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
 
   }
 }
