df93b875ad2b74bf16430d4141b306210b51a911
boomtodoom <65396048+boomtodoom@users.noreply.github.com>
2021-11-13 18:57:53 +0000

    Fixed sams stupid error part 99

--- EncrypredChatProgram/src/BackEnd.java
+++ EncrypredChatProgram/src/BackEnd.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,27 +12,43 @@ public class BackEnd {
 
 
   public static void main(String[] args) throws IOException {
-      Scanner scan = new Scanner(System.in);
-      System.out.println("Please enter the ip you wish to connect to: ");
-      String ip = scan.nextLine();
-      InetAddress iAddress = InetAddress.getByName(ip);
-      System.out.println("Please enter the port you wish to connect to: ");
-      int port = scan.nextInt();
-      Socket socket = new Socket(iAddress, port);
-      System.out.println("Please enter a port you want open");
-      int openPort = scan.nextInt();
-     //poopoo pee pee
+    BackEnd back = new BackEnd();
+    back.initialise();
+  }
 
-      ServerSocket sSoc = new ServerSocket(openPort);
-      Socket newSoc = sSoc.accept();
+  public void initialise() throws IOException {
+    /**
+     * Gets the port and ip of the client to connect to and then creates a socket
+     */
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please enter the ip you wish to connect to: ");
+    String ip = scan.nextLine();
+    InetAddress iAddress = InetAddress.getByName(ip);
+    System.out.println("Please enter the port you wish to connect to: ");
+    int port = scan.nextInt();
+    Socket socket = new Socket(iAddress, port);
+    sockArray.add(socket);
+    /**
+     * Starts a thread that creates new sockets every time a user connects to this client
+     */
+    System.out.println("Please enter a port you want open");
+    int openPort = scan.nextInt();
+    ServerSocket sSoc = new ServerSocket(openPort);
+    ListenSocCraft lSoc = new ListenSocCraft(); //add server socket
+    lSoc.run();
 
-      Send initMsg = new Send(socket);
-      System.out.println("Please enter your desired username");
-      String uName = scan.nextLine();
-      initMsg.send("INIT::"+uName); // remove dogshit code
-      //Receive list of sockets
-
-
-
+    /**
+     * Loops infinitely checking for the users message and then sending them to all connected clients
+     */
+    while(true){
+      System.out.println("Please enter the message: ");
+      String message = scan.nextLine();
+      for(int i=0; i<senderArray.size();i++){
+        senderArray.get(i).send(message);
+      }
+    }
+    /**
+     *
+     */
   }
 }
