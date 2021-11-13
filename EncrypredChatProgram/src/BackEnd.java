5ab1dad4eefe9fa4166600166dd016b3f090cc5c
boomtodoom <65396048+boomtodoom@users.noreply.github.com>
2021-11-13 17:41:53 +0000

    Fixed sams stupid error pt5

--- EncrypredChatProgram/src/BackEnd.java
+++ EncrypredChatProgram/src/BackEnd.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -6,10 +7,19 @@ public class BackEnd {
   private ArrayList<Socket> sockArray = new ArrayList<>();
   private ArrayList<Send> senderArray = new ArrayList<>();
 
-  public static void main(String[] args){
+  public static void main(String[] args) throws IOException {
     Scanner scan = new Scanner(System.in);
     System.out.println("Please enter the ip you wish to connect to: ");
     String ip = scan.nextLine();
+    System.out.println("Please enter the port you wish to connect to: ");
+    int port = scan.nextInt();
+    Socket socket = new Socket(ip,port);
+    Send initMsg = new Send(socket);
+    System.out.println("Please enter your desired username");
+    String uName = scan.nextLine();
+    initMsg.send("INIT::"+uName);
+    //Receive list of sockets
+
 
 
   }
